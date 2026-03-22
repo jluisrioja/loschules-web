f9551521240d69e090121ceda3f10b11f801072c
jluisrioja <Jorge.luis.rioja.saldivar@gmail.com>
2026-03-22 01:27:35 -0500

    Agrega sección de primera creación

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import Hero from "@/components/home/Hero";
+import EcosystemSection from "@/components/home/EcosystemSection";
+import FeaturedProductSection from "@/components/home/FeaturedProductSection";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <EcosystemSection />
+      <FeaturedProductSection />
+    </>
+  );
 }
